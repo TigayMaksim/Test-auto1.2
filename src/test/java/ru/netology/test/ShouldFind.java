2fc3a14ebb360dcd6c10c8c1a49cad2b9b350004
Тигай Максим <timati.87@list.ru>
2022-08-03 15:41:46 +0300

    first

---
+++ src/test/java/ru/netology/test/ShouldFind.java
@@ -0,0 +1,29 @@
+package ru.netology.test;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ShouldFind {
+    @Test
+    public void ShouldFind(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Школа Нетология") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Школа Нетология"));
+
+
+
+    }
+}
